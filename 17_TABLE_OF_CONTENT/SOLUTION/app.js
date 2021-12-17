b3c2115bf16bd4847cd94814ecc979d57bcdc8cb
fabianrguez <fabian1041993@gmail.com>
2021-12-17 15:47:43 +0000

    challenge 17 solved

---
+++ 17_TABLE_OF_CONTENT/SOLUTION/app.js
@@ -0,0 +1,33 @@
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+const titles = document.querySelectorAll('h3');
+
+function setNavLinkActive(hash) {
+  console.log({ hash });
+  document.querySelectorAll('aside ul li').forEach((li) => li.classList.remove('selected'));
+  const anchor = [...anchorLinks].find((anchor) => anchor.hash === hash);
+  anchor.parentElement.classList.add('selected');
+}
+
+function contentScrolledObserver(element) {
+  const options = {
+    rootMargin: '-150px 0px',
+    threshold: 1.0,
+  };
+  const observer = new IntersectionObserver((intersection) => {
+    const [item] = intersection;
+    if (item.isIntersecting) {
+      setNavLinkActive(`#${element.id}`);
+    }
+  }, options);
+  observer.observe(element);
+}
+
+function scrollToContent(e) {
+  e.preventDefault();
+  const element = document.querySelector(this.hash);
+  element.scrollIntoView({ behaviour: 'smooth' });
+  setNavLinkActive(this.hash)
+}
+
+titles.forEach((title) => contentScrolledObserver(title));
+anchorLinks.forEach((anchorLink) => anchorLink.addEventListener('click', scrollToContent));
